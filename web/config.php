ebb44db7b8e059b615028ee2cafb49f82ed6a02c
Kendrick Chan <kc@jankstudio.com>
2013-09-12 03:53:09 +0800

    removed localhost restriction for config.php to test if dev server is ok temporary fix, we have to put restriction back later on for security purposes

--- web/config.php
+++ web/config.php
@@ -4,6 +4,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
+/*
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
@@ -11,6 +12,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
 }
+*/
 
 require_once dirname(__FILE__).'/../app/SymfonyRequirements.php';
 
