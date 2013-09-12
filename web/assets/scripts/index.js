9b5d19fa4d47c3043a9775fdb7992221402ec387
Kendrick Chan <kc@jankstudio.com>
2013-09-13 01:28:44 +0800

    added admin assets added admin.html and admin-login.html to test assets

---
+++ web/assets/scripts/index.js
@@ -0,0 +1,299 @@
+var Index = function () {
+
+
+    return {
+
+        //main function to initiate the module
+        init: function () {
+
+            App.addResponsiveHandler(function () {
+                Index.initCalendar();
+                jQuery('.vmaps').each(function () {
+                    var map = jQuery(this);
+                    map.width(map.parent().width());
+                });
+            });
+        },
+
+
+        initCharts: function () {
+            if (!jQuery.plot) {
+                return;
+            }
+
+            var data = [];
+            var totalPoints = 250;
+
+            // random data generator for plot charts
+
+            function showTooltip(title, x, y, contents) {
+                $('<div id="tooltip" class="chart-tooltip"><div class="date">' + title + '<\/div><div class="label label-success">CTR: ' + x / 10 + '%<\/div><div class="label label-important">Imp: ' + x * 12 + '<\/div><\/div>').css({
+                    position: 'absolute',
+                    display: 'none',
+                    top: y - 100,
+                    width: 75,
+                    left: x - 40,
+                    border: '0px solid #ccc',
+                    padding: '2px 6px',
+                    'background-color': '#fff',
+                }).appendTo("body").fadeIn(200);
+            }
+
+            function randValue() {
+                return (Math.floor(Math.random() * (1 + 50 - 20))) + 10;
+            }
+
+            var uniqueCalls = [
+                [1, randValue()],
+                [2, randValue()],
+                [3, 2 + randValue()],
+                [4, 3 + randValue()],
+                [5, 5 + randValue()]/*,
+                [6, 10 + randValue()],
+                [7, 15 + randValue()],
+                [8, 20 + randValue()],
+                [9, 25 + randValue()],
+                [10, 30 + randValue()],
+                [11, 35 + randValue()],
+                [12, 25 + randValue()],
+                [13, 15 + randValue()],
+                [14, 20 + randValue()],
+                [15, 45 + randValue()],
+                [16, 50 + randValue()],
+                [17, 65 + randValue()],
+                [18, 70 + randValue()],
+                [19, 85 + randValue()],
+                [20, 80 + randValue()],
+                [21, 75 + randValue()],
+                [22, 80 + randValue()],
+                [23, 75 + randValue()],
+                [24, 70 + randValue()],
+                [25, 65 + randValue()],
+                [26, 75 + randValue()],
+                [27, 80 + randValue()],
+                [28, 85 + randValue()],
+                [29, 90 + randValue()],
+                [30, 95 + randValue()]*/
+            ];
+
+            var totalCalls = [
+                [1, randValue() - 5],
+                [2, randValue() - 5],
+                [3, randValue() - 5],
+                [4, 6 + randValue()],
+                [5, 5 + randValue()]/*,
+                [6, 20 + randValue()],
+                [7, 25 + randValue()],
+                [8, 36 + randValue()],
+                [9, 26 + randValue()],
+                [10, 38 + randValue()],
+                [11, 39 + randValue()],
+                [12, 50 + randValue()],
+                [13, 51 + randValue()],
+                [14, 12 + randValue()],
+                [15, 13 + randValue()],
+                [16, 14 + randValue()],
+                [17, 15 + randValue()],
+                [18, 15 + randValue()],
+                [19, 16 + randValue()],
+                [20, 17 + randValue()],
+                [21, 18 + randValue()],
+                [22, 19 + randValue()],
+                [23, 20 + randValue()],
+                [24, 21 + randValue()],
+                [25, 14 + randValue()],
+                [26, 24 + randValue()],
+                [27, 25 + randValue()],
+                [28, 26 + randValue()],
+                [29, 27 + randValue()],
+                [30, 31 + randValue()]*/
+            ];
+
+						var data1 = [
+						      {
+						          label: "Unique Calls",
+						          data: uniqueCalls,
+						          bars: {
+						              show: true,
+						              //barWidth: 12*24*60*60*1,
+						              fill: true,
+						              order: 1,
+						              fillColor:  "#AA4643"
+						          },
+						          color: "#AA4643"
+						      },
+						      {
+					            label: "Total Calls",
+					            data: totalCalls,
+					            bars: {
+					                show: true,
+					                //barWidth: 12*24*60*60*300,
+					                fill: true,
+					                order: 2,
+					                fillColor:  "#89A54E"
+					            },
+					            color: "#89A54E"
+					        }
+					 ];
+            if ($('#site_statistics').size() != 0) {
+
+                $('#site_statistics_loading').hide();
+                $('#site_statistics_content').show();
+
+                var plot_statistics = $.plot($("#site_statistics"), data1, {
+
+                    xaxis: {
+                        ticks: 31,
+                        tickDecimals: 0
+                    },
+                    yaxis: {
+                        ticks: 3,
+                        tickDecimals: 0
+                    }
+                });
+
+                var previousPoint = null;
+                $("#site_statistics").bind("plothover", function (event, pos, item) {
+                    $("#x").text(pos.x.toFixed(2));
+                    $("#y").text(pos.y.toFixed(2));
+                    if (item) {
+                        if (previousPoint != item.dataIndex) {
+                            previousPoint = item.dataIndex;
+
+                            $("#tooltip").remove();
+                            var x = item.datapoint[0].toFixed(2),
+                                y = item.datapoint[1].toFixed(2);
+
+                            showTooltip('24 Jan 2013', item.pageX, item.pageY, item.series.label + " of " + x + " = " + y);
+                        }
+                    } else {
+                        $("#tooltip").remove();
+                        previousPoint = null;
+                    }
+                });
+            }               
+
+            if ($('#load_statistics').size() != 0) {
+                 //server load
+                $('#load_statistics_loading').hide();
+                $('#load_statistics_content').show();
+        
+                var updateInterval = 30;
+                var plot_statistics = $.plot($("#load_statistics"), [getRandomData()], {
+                series: {
+                    shadowSize: 1
+                },
+                lines: {
+                    show: true,
+                    lineWidth: 0.2,
+                    fill: true,
+                    fillColor: {
+                        colors: [{
+                                opacity: 0.1
+                            }, {
+                                opacity: 1
+                            }
+                        ]
+                    }
+                },
+                yaxis: {
+                    min: 0,
+                    max: 100,
+                    tickFormatter: function (v) {
+                        return v + "%";
+                    }
+                },
+                xaxis: {
+                    show: false
+                },
+                colors: ["#e14e3d"],
+                grid: {
+                    tickColor: "#a8a3a3",
+                    borderWidth: 0
+                }
+                });
+                
+                function statisticsUpdate() {
+                plot_statistics.setData([getRandomData()]);
+                plot_statistics.draw();
+                setTimeout(statisticsUpdate, updateInterval);
+                
+                }
+                
+                statisticsUpdate();
+
+                $('#load_statistics').bind("mouseleave", function () {
+                    $("#tooltip").remove();
+                });
+            }
+
+        },
+
+
+
+        initDashboardDaterange: function () {
+
+            $('#dashboard-report-range').daterangepicker({
+                ranges: {
+                    'Today': ['today', 'today'],
+                    'Yesterday': ['yesterday', 'yesterday'],
+                    'Last 7 Days': [Date.today().add({
+                            days: -6
+                        }), 'today'],
+                    'Last 30 Days': [Date.today().add({
+                            days: -29
+                        }), 'today'],
+                    'This Month': [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
+                    'Last Month': [Date.today().moveToFirstDayOfMonth().add({
+                            months: -1
+                        }), Date.today().moveToFirstDayOfMonth().add({
+                            days: -1
+                        })]
+                },
+                opens: (App.isRTL() ? 'right' : 'left'),
+                format: 'MM/dd/yyyy',
+                separator: ' to ',
+                startDate: Date.today().add({
+                    days: -29
+                }),
+                endDate: Date.today(),
+                minDate: '01/01/2012',
+                maxDate: '12/31/2014',
+                locale: {
+                    applyLabel: 'Submit',
+                    fromLabel: 'From',
+                    toLabel: 'To',
+                    customRangeLabel: 'Custom Range',
+                    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+                    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                    firstDay: 1
+                },
+                showWeekNumbers: true,
+                buttonClasses: ['btn-danger']
+            },
+
+            function (start, end) {
+                App.blockUI(jQuery("#dashboard"));
+                setTimeout(function () {
+                    App.unblockUI(jQuery("#dashboard"));
+                    $.gritter.add({
+                        title: 'Dashboard',
+                        text: 'Dashboard date range updated.'
+                    });
+                    App.scrollTo();
+                }, 1000);
+                $('#dashboard-report-range span').html(start.toString('MMMM d, yyyy') + ' - ' + end.toString('MMMM d, yyyy'));
+
+            });
+
+            $('#dashboard-report-range').show();
+
+            $('#dashboard-report-range span').html(Date.today().add({
+                days: -29
+            }).toString('MMMM d, yyyy') + ' - ' + Date.today().toString('MMMM d, yyyy'));
+        }
+
+
+    };
+
+}();
