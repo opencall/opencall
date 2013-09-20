f5b94a09887c35cb8181d4c92a2b657d0d7bdc92
Kendrick Chan <kc@jankstudio.com>
2013-09-20 22:22:24 +0800

    added ajaxForm to base template added add new account functionality in account controller added form tag for add new account in accounts view added alters.sql for easy update of db

--- src/OnCall/Bundle/AdminBundle/Controller/AccountController.php
+++ src/OnCall/Bundle/AdminBundle/Controller/AccountController.php
@@ -4,6 +4,7 @@ namespace OnCall\Bundle\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use OnCall\Bundle\AdminBundle\Model\MenuHandler;
+use Symfony\Component\HttpFoundation\Response;
 
 class AccountController extends Controller
 {
@@ -16,5 +17,64 @@ class AccountController extends Controller
             )
         );
 
+    }
+
+    public function createAction()
+    {
+        $mgr = $this->get('fos_user.user_manager');
+        $user = $mgr->createUser();
+
+        $req = $this->getRequest();
+        /*
+        $form = $this->createFormBuilder($user)
+            ->add('multi_client', 'checkbox')
+            ->add('username', 'text')
+            ->add('password', 'password')
+            ->add('name', 'text')
+            ->add('email', 'text')
+            ->add('business_name', 'text')
+            ->add('phone', 'text')
+            ->add('address', 'text')
+            ->add('bill_business_name', 'text')
+            ->add('bill_name', 'text')
+            ->add('bill_email', 'text')
+            ->add('bill_phone', 'text')
+            ->add('bill_address', 'text')
+            ->add('enabled', 'choice', array(
+                'choices' => array(
+                    '1' => 'Active',
+                    '0' => 'Disabled'
+                )
+            ))
+            ->getForm();
+
+        $form->handleRequest($req);
+        */
+
+        $data = $req->request->all();
+        $user->setUsername($data['username'])
+            ->setPlainPassword($data['password'])
+            ->setName($data['name'])
+            ->setEmail($data['email'])
+            ->setBusinessName($data['business_name'])
+            ->setPhone($data['phone'])
+            ->setAddress($data['address'])
+            ->setBillBusinessName($data['bill_business_name'])
+            ->setBillName($data['bill_name'])
+            ->setBillEmail($data['bill_email'])
+            ->setBillPhone($data['bill_phone'])
+            ->setBillAddress($data['bill_address'])
+            ->setEnabled($data['enabled'])
+            ->setRoles(array('ROLE_USER'));
+        $mgr->updateUser($user);
+        return $this->redirect($this->generateUrl('oncall_admin_accounts'));
+    }
+
+    public function find()
+    {
+    }
+
+    public function update()
+    {
     }
 }
--- src/OnCall/Bundle/UserBundle/Entity/User.php
+++ src/OnCall/Bundle/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace OnCall\Bundle\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use DateTime;
 
 /**
  * @ORM\Entity
@@ -18,9 +19,181 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $multi_client;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    protected $business_name;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    protected $phone;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    protected $address;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    protected $bill_business_name;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    protected $bill_name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    protected $bill_email;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    protected $bill_phone;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    protected $bill_address;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $date_create;
+
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+
+        // timestamp
+        $this->date_create = new DateTime();
     }
+
+    // begin setters
+    public function setMultiClient($multi = true)
+    {
+        $this->multi_client = $multi;
+        return $this;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setBusinessName($name)
+    {
+        $this->business_name = $name;
+        return $this;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function setBillBusinessName($name)
+    {
+        $this->bill_busines_name = $name;
+        return $this;
+    }
+
+    public function setBillName($name)
+    {
+        $this->bill_name = $name;
+        return $this;
+    }
+
+    public function setBillEmail($email)
+    {
+        $this->bill_email = $email;
+        return $this;
+    }
+
+    public function setBillPhone($phone)
+    {
+        $this->bill_phone = $phone;
+        return $this;
+    }
+    
+    public function setBillAddress($address)
+    {
+        $this->bill_address = $address;
+        return $this;
+    }
+    // end setters
+
+    // begin getters
+    public function isMultiClient()
+    {
+        return $this->multi_client;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getBusinessName()
+    {
+        return $this->business_name;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function getBillBusinessName()
+    {
+        return $this->bill_business_name;
+    }
+
+    public function getBillName()
+    {
+        return $this->bill_name;
+    }
+
+    public function getBillEmail()
+    {
+        return $this->bill_email;
+    }
+
+    public function getBillPhone()
+    {
+        return $this->bill_phone;
+    }
+
+    public function getBillAddress()
+    {
+        return $this->bill_address;
+    }
+    // end getters
 }
