5ecbd5b9ef33dd0690f900bcfbace566666199de
Kendrick Chan <kc@jankstudio.com>
2013-09-30 05:45:55 +0800

    setup default redirects for front page for admin and normal user roles

--- src/OnCall/Bundle/AdminBundle/Controller/MainController.php
+++ src/OnCall/Bundle/AdminBundle/Controller/MainController.php
@@ -9,12 +9,13 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        return $this->render(
-            'OnCallAdminBundle:Main:index.html.twig',
-            array(
-                'sidebar_menu' => MenuHandler::getMenu($this->getUser()->getRoleHash())
-            )
-        );
+        $role_hash = $this->getUser()->getRoleHash();
 
+        // check if admin
+        if (isset($role_hash['ROLE_ADMIN']))
+            return $this->redirect($this->generateUrl('oncall_admin_accounts'));
+
+        // everyone else
+        return $this->redirect($this->generateUrl('oncall_admin_clients'));
     }
 }
