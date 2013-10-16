88a464847ef26e472a1ddc115f7d42290c22aa2e
Kendrick Chan <kc@jankstudio.com>
2013-10-17 05:50:14 +0800

    refactored actual answer and hangup scripts to use the new Answer and Hangup classes

--- web/plivo/answer.php
+++ web/plivo/answer.php
@@ -3,88 +3,27 @@
 require_once(__DIR__ . '/../../app/autoload.php');
 
 use Predis\Client as PredisClient;
-use Plivo\Queue\Message as QMessage;
-use Plivo\Parameters;
-use Plivo\Response;
-use Plivo\Router;
-use Plivo\Action;
+use Plivo\Answer;
 
-try
-{
-    // redis prefix
-    $prefix = 'plivo:ongoing:';
+// setup redis
+$rconf = array(
+    'scheme' => 'tcp',
+    'host' => 'devredisnode.5zaozk.0001.apse1.cache.amazonaws.com',
+    'port' => 6379
+);
+$redis = new PredisClient($rconf);
 
-    // setup redis
-    $rconf = array(
-        'scheme' => 'tcp',
-        'host' => 'devredisnode.5zaozk.0001.apse1.cache.amazonaws.com',
-        'port' => 6379
-    );
-    $redis = new PredisClient($rconf);
+// setup mysql
+$dsn = 'mysql:host=db.oncall;dbname=oncall';
+$user = 'webuser';
+$pass = 'lks8jw23';
+$pdo = new PDO($dsn, $user, $pass);
 
-    /*
-    // NOTE: local redis
-    $redis = new PredisClient();
-    */
+// zeromq
+$zmq_server = 'tcp://localhost:5555';
+$context = new ZMQContext();
+$zmq_socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'log_pusher');
+$zmq_socket->connect($zmq_server);
 
-    // setup mysql
-    $dsn = 'mysql:host=db.oncall;dbname=oncall';
-    $user = 'webuser';
-    $pass = 'lks8jw23';
-    $pdo_main = new PDO($dsn, $user, $pass);
-
-    // TODO: fallback mysql setup
-
-    // parse parameters
-    $params = new Parameters($_POST);
-
-    // get response based on params
-    $router = new Router($pdo_main);
-    $response = $router->resolve($params);
-    $num_data = $router->getNumberData();
-
-    // store response xml
-    $xml = $response->renderXML();
-
-    // setup queue message
-    $qmsg = new QMessage();
-    $qmsg->setAnswerParams($params);
-    $qmsg->setNumberData($num_data);
-    $qmsg->setResponseXML($xml);
-    $serial_qmsg = serialize($qmsg);
-
-    // add as ongoing call to redis
-    $key = $prefix . $params->getUniqueID();
-    $redis->set($key, $serial_qmsg);
-
-    // output XML
-    echo $xml;
-}
-catch (\Predis\Connection\ConnectionException $e)
-{
-    // catch redis error
-    error_log('redis exception');
-    $act_params = array(
-        'language' => 'en-GB',
-        'text' => 'There was a problem connecting your call. This error has been logged and we will rectify the problem as soon as possible.'
-    );
-    $response = new Response();
-    $action = new Action(Action::TYPE_SPEAK, $act_params);
-    $response->addAction($action);
-
-    echo $response->renderXML();
-}
-catch (PDOException $e)
-{
-    // catch pdo / db error
-    error_log('pdo exception');
-    $act_params = array(
-        'language' => 'en-GB',
-        'text' => 'There was a problem connecting your call. This error has been logged and we will rectify the problem as soon as possible.'
-    );
-    $response = new Response();
-    $action = new Action(Action::TYPE_SPEAK, $act_params);
-    $response->addAction($action);
-
-    echo $response->renderXML();
-}
+$answer = new Answer($pdo, $redis, $zmq_socket);
+echo $answer->run($_POST);
--- web/plivo/hangup.php
+++ web/plivo/hangup.php
@@ -3,88 +3,31 @@
 require_once(__DIR__ . '/../../app/autoload.php');
 
 use Predis\Client as PredisClient;
-use Plivo\Queue\Message as QMessage;
-use Plivo\Queue\Handler as QHandler;
-use Plivo\Parameters;
-use Plivo\Response;
-use Plivo\Router;
-use Plivo\Action;
-use Plivo\Log\Entry as LogEntry;
-use Plivo\Log\Repository as LogRepository;
-use Plivo\Aggregate\Entry as AggEntry;
-use Plivo\Aggregate\Repository as AggRepository;
+use Plivo\Hangup;
+
+// setup redis
+$rconf = array(
+    'scheme' => 'tcp',
+    'host' => 'devredisnode.5zaozk.0001.apse1.cache.amazonaws.com',
+    'port' => 6379
+);
+$redis = new PredisClient($rconf);
+
+// setup mysql
+$dsn = 'mysql:host=db.oncall;dbname=oncall';
+$user = 'webuser';
+$pass = 'lks8jw23';
+$pdo = new PDO($dsn, $user, $pass);
+
+// zeromq
+$zmq_server = 'tcp://localhost:5555';
+$context = new ZMQContext();
+$zmq_socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'log_pusher');
+$zmq_socket->connect($zmq_server);
 
 
-try
-{
-    // redis prefix
-    $prefix = 'plivo:ongoing:';
-    $queue_id = 'plivo_log';
-
-    // setup redis
-    $rconf = array(
-        'scheme' => 'tcp',
-        'host' => 'devredisnode.5zaozk.0001.apse1.cache.amazonaws.com',
-        'port' => 6379
-    );
-    $redis = new PredisClient($rconf);
-
-    // setup mysql
-    $dsn = 'mysql:host=db.oncall;dbname=oncall';
-    $user = 'webuser';
-    $pass = 'lks8jw23';
-    $pdo_main = new PDO($dsn, $user, $pass);
-
-    // TODO: fallback mysql setup
-
-    // parse parameters
-    $params = new Parameters($_POST);
-
-    // get ongoing call data from redis
-    $key = $prefix . $params->getUniqueID();
-    $raw_qmsg = $redis->get($key);
-    if ($raw_qmsg == null)
-    {
-        // TODO: what to do when there's no matching answer param
-        error_log('no previous answer parameters');
-        exit();
-    }
-    $qmsg = unserialize($raw_qmsg);
-    $qmsg->setHangupParams($params);
-
-    // delete key
-    $redis->del($key);
-
-    // start log and aggregate
-    // NOTE: this version is live, no queueing
-
-    // log repo
-    $log_repo = new LogRepository($pdo_main);
-
-    // aggregate repo
-    $agg_repo = new AggRepository($pdo_main);
-
-    // log
-    $log = LogEntry::createFromMessage($qmsg);
-    $log_repo->persist($log);
-
-    // aggregate
-    $agg = AggEntry::createFromMessage($qmsg);
-    $agg_repo->persist($agg);
-
-    // TODO: send zmq message to publish
-
-    // end log and aggregate
-}
-catch (\Predis\Connection\ConnectionException $e)
-{
-    // catch redis error
-    error_log('redis exception');
-}
-catch (PDOException $e)
-{
-    // catch pdo / db error
-    error_log('pdo exception');
-}
+// hangup
+$hangup = new Hangup($pdo, $redis, $zmq_socket);
+$hangup->run($_POST);
 
 echo '';
