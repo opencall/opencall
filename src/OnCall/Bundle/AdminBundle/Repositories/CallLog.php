001594118ab651fd33f39a6af0997cc4b565e8ad
Kendrick Chan <kc@jankstudio.com>
2013-10-26 01:03:59 +0800

    did some refactoring for call log filters

--- src/OnCall/Bundle/AdminBundle/Repositories/CallLog.php
+++ src/OnCall/Bundle/AdminBundle/Repositories/CallLog.php
@@ -7,21 +7,8 @@ use Plivo\Log\Filter;
 
 class CallLog extends EntityRepository
 {
-    public function findLatest($client_id, Filter $filter, $last_id = null, $limit = 10, $offset = 0)
+    protected function filterItem(Filter $filter, $qb)
     {
-        // create query and set client_id
-        $qb = $this->createQueryBuilder('cl')
-            ->where('cl.client_id = :client_id')
-            ->setParameter('client_id', $client_id);
-
-        // last id
-        if ($last_id != null)
-        {
-            $qb->andWhere('cl.id < :last_id')
-                ->setParameter('last_id', $last_id);
-        }
-
-        // filters
         // campaign
         if ($filter->getCID() != null)
         {
@@ -42,14 +29,10 @@ class CallLog extends EntityRepository
             $qb->andWhere('cl.advert_id = :advert_id')
                 ->setParameter('advert_id', $filter->getAdID());
         }
+    }
 
-        // hangup cause
-        if ($filter->getHCause() != null)
-        {
-            $qb->andWhere('cl.hangup_cause = :hangup_cause')
-                ->setParameter('hangup_cause', $filter->getHCause());
-        }
-
+    protected function filterDuration(Filter $filter, $qb)
+    {
         // duration
         $dmod = $filter->getDMod();
         if ($dmod != null && $filter->getDSec() != null)
@@ -71,7 +54,10 @@ class CallLog extends EntityRepository
             $qb->andWhere('cl.duration ' . $dmod_string . ' :duration')
                 ->setParameter('duration', $filter->getDSec());
         }
+    }
 
+    protected function filterNumber(Filter $filter, $qb)
+    {
         // number
         if ($filter->getNum() != null)
         {
@@ -79,13 +65,48 @@ class CallLog extends EntityRepository
             $qb->andWhere('cl.dialled_number like :number or cl.origin_number like :number or cl.destination_number like :number')
                 ->setParameter('number', '%' . $num . '%');
         }
+    }
 
+    protected function filterFailed(Filter $filter, $qb)
+    {
         // failed
         if ($filter->isFailed())
         {
             $qb->andWhere('cl.status in (:status)')
                 ->setParameter('status', array('busy', 'failed', 'timeout', 'no-answer', 'cancel'));
         }
+    }
+
+    protected function filterHCause(Filter $filter, $qb)
+    {
+        // hangup cause
+        if ($filter->getHCause() != null)
+        {
+            $qb->andWhere('cl.hangup_cause = :hangup_cause')
+                ->setParameter('hangup_cause', $filter->getHCause());
+        }
+    }
+
+    public function findLatest($client_id, Filter $filter, $last_id = null, $limit = 10, $offset = 0)
+    {
+        // create query and set client_id
+        $qb = $this->createQueryBuilder('cl')
+            ->where('cl.client_id = :client_id')
+            ->setParameter('client_id', $client_id);
+
+        // last id
+        if ($last_id != null)
+        {
+            $qb->andWhere('cl.id < :last_id')
+                ->setParameter('last_id', $last_id);
+        }
+
+        // filters
+        $this->filterItem($filter, $qb);
+        $this->filterDuration($filter, $qb);
+        $this->filterNumber($filter, $qb);
+        $this->filterFailed($filter, $qb);
+        $this->filterHCause($filter, $qb);
 
         // order and limit
         $qb->orderBy('cl.id', 'DESC')
