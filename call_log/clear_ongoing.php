135396ee9847a51f061221b27cb47d5705e00f9e
Kendrick Chan <kc@jankstudio.com>
2013-11-13 09:23:35 +0800

    adding script for clearing bugged ongoing calls

---
+++ call_log/clear_ongoing.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(__DIR__ . '/../app/autoload.php');
+
+use Predis\Client as PredisClient;
+
+// setup redis
+$rconf = array(
+    'scheme' => 'tcp',
+    'host' => 'devredisnode.5zaozk.0001.apse1.cache.amazonaws.com',
+    'port' => 6379
+);
+$redis = new PredisClient($rconf);
+
+
+$keys = $redis->keys('plivo:ongoing*');
+foreach ($keys as $key)
+{
+    echo "deleting $key\n";
+}
