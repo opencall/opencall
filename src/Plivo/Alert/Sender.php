7e0b196ef0dc3125e66fdd103affa844acec0339
Kendrick Chan <kc@jankstudio.com>
2014-02-19 09:40:37 +0800

    fixed email template bug

--- src/Plivo/Alert/Sender.php
+++ src/Plivo/Alert/Sender.php
@@ -42,9 +42,9 @@ class Sender
         $ftext = str_replace('[dialled_number]', $log->getDialledFormatted(), $ftext);
         $ftext = str_replace('[reason]', $log->getBHangupCause(), $ftext);
         $ftext = str_replace('[lead_rescue_url]', 'http://dev.calltracking.hk/client/' . $log->getClientID() . '/lead_rescue', $ftext);
-        $ftext = str_replace('[advert]', $this->fetchItemName('Advert', $log->getAdvertID()));
-        $ftext = str_replace('[adgroup]', $this->fetchItemName('AdGroup', $log->getAdvertID()));
-        $ftext = str_replace('[campaign]', $this->fetchItemName('Campaign', $log->getAdvertID()));
+        $ftext = str_replace('[advert]', $this->fetchItemName('Advert', $log->getAdvertID()), $ftext);
+        $ftext = str_replace('[adgroup]', $this->fetchItemName('AdGroup', $log->getAdvertID()), $ftext);
+        $ftext = str_replace('[campaign]', $this->fetchItemName('Campaign', $log->getAdvertID()), $ftext);
 
         return $ftext;
     }
